9ea5b065819b5cb003f28e677494d4a13e7ab32a
Carrington Manyuchi <carrington.manyuchi@standardbank.co.za>
2024-08-23 11:42:03 +0200

    Start

---
+++ NavigationBootcamp/NavigationBootcamp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NavigationBootcamp
+//
+//  Created by Manyuchi, Carrington C on 2024/08/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NavigationBootcamp/NavigationBootcamp/NavigationBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationBootcampApp.swift
+//  NavigationBootcamp
+//
+//  Created by Manyuchi, Carrington C on 2024/08/23.
+//
+
+import SwiftUI
+
+@main
+struct NavigationBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
